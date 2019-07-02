96560287c16ddb4500e449da47057106f03b175a
adilux722 <52382567+adilux722@users.noreply.github.com>
2019-07-02 14:48:01 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+          console.log('Im Ready!');
+});
+
+client.on("message", message => {
+    if (message.content === 'ping') {
+       message.reply('pong');
+    }
+)};
+
+// THIS MUSTE BE THIS WAY
+kboosh.login(process.env.BOT_TOKEN);
+                   
+          
